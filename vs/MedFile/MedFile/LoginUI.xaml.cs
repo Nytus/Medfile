e1bc220c3e64462c07b2aa34a1a4327243f01280
xNytus <s.farasjiles@uandresbello.edu>
2022-11-22 09:52:42 -0300

    Agregado parte del desarrollo del proyecto

---
+++ vs/MedFile/MedFile/LoginUI.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MedFile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (txtUsername.Text=="11.111.111-1" && txtPassword.Text=="123")
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+            else
+            {
+                DisplayAlert("Ops..", "Rut o contraseña incorrectos","ok");
+            }
+
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ContraseñaperdidaPage());
+        }
+    }
+}
